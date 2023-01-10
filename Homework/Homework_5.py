0fc7039ca25749df68a190e34117f682df6709a6
vitalii <vitalii13a@gmail.com>
2023-01-09 22:02:20 -0500

    Homework_5

--- Homework/Homework_5.py
+++ Homework/Homework_5.py
@@ -3,7 +3,7 @@
 # Задание выполнить с помощью цикла for.
 #
 from random import randint
-my_list = [randint(1, 1000) for i in range(10)]
+my_list = [randint(3, 300) for i in range(30)]
 for element in my_list:
     if element > 100:
         print(element, end=' ')
@@ -15,7 +15,7 @@ print('Task_1')
 # Распечатать список my_results.
 # Задание выполнить с помощью цикла for.
 
-my_list = [randint(1,300) for i in range(20)]
+my_list = [randint(1,300) for i in range(30)]
 my_results = []
 for n in my_list:
         if n > 100:
@@ -27,7 +27,7 @@ print('Task_2')
 # Если количество элементов больше или равно 2, то добавить сумму последних двух элементов.
 # Количество элементов в списке можно получить с помощью функции len(my_list)
 
-my_list = [randint(3,300) for i in range(randint(1,10))]
+my_list = [randint(3,300) for i in range(randint(1,30))]
 if len(my_list)<2:
     my_list.append(0)
 elif len(my_list)>=2:
